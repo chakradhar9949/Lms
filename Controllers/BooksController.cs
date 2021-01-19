8971d2372a266e3341c91b0ba7277236be32c417
Chakradhar Peddi <Chakradhar.p@keka.com>
2021-01-19 16:34:39 +0530

    Add project files.

---
+++ Controllers/BooksController.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Lms.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace Lms.Controllers
+{
+    /// <summary>
+    /// The books controller.
+    /// </summary>
+    /// <seealso cref="Microsoft.AspNetCore.Mvc.ControllerBase" />
+    [Route("api/books")]
+    public class BooksController : ControllerBase
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="BooksController"/> class.
+        /// </summary>
+        public BooksController()
+        {
+        }
+
+        /// <summary>
+        /// Gets all books.
+        /// </summary>
+        /// <returns>The all books.</returns>
+        [Route("all")]
+        public IEnumerable<Book> GetAllBooks()
+        {
+            return new List<Book>();
+        }
+
+        /// <summary>
+        /// Gets the available books.
+        /// </summary>
+        /// <returns>The available books.</returns>
+        [Route("available")]
+        public IEnumerable<Book> GetAvailableBooks()
+        {
+            return new List<Book>();
+        }
+
+        /// <summary>
+        /// Posts the book.
+        /// </summary>
+        /// <param name="book">The book.</param>
+        /// <returns>The http response message.</returns>
+        [Route("save")]
+        public HttpResponseMessage PostBook(Book book)
+        {
+            return new HttpResponseMessage(HttpStatusCode.OK);
+        }
+
+        /// <summary>
+        /// Puts the book.
+        /// </summary>
+        /// <param name="id">The identifier.</param>
+        /// <param name="book">The book.</param>
+        /// <returns>
+        /// The http response message.
+        /// </returns>
+        [Route("{id}/update")]
+        public HttpResponseMessage PutBook(int id, Book book)
+        {
+            return new HttpResponseMessage(HttpStatusCode.OK);
+        }
+
+        /// <summary>
+        /// Deletes the book.
+        /// </summary>
+        /// <param name="id">The identifier.</param>
+        /// <returns>The http response message.</returns>
+        [Route("{id}/delete")]
+        public HttpResponseMessage DeleteBook(int id)
+        {
+            return new HttpResponseMessage(HttpStatusCode.OK);
+        }
+
+        /// <summary>
+        /// Deletes all books.
+        /// </summary>
+        /// <returns>The http response message.</returns>
+        [Route("delete/all")]
+        public HttpResponseMessage DeleteAllBooks()
+        {
+            return new HttpResponseMessage(HttpStatusCode.OK);
+        }
+    }
+}
---
+++ Models/Book.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Lms.Models
+{
+    /// <summary>
+    /// The book
+    /// </summary>
+    public class Book
+    {
+        /// <summary>
+        /// Gets or sets the identifier.
+        /// </summary>
+        /// <value>
+        /// The identifier.
+        /// </value>
+        public int Id { get; set; }
+
+        /// <summary>
+        /// Gets or sets the name.
+        /// </summary>
+        /// <value>
+        /// The name.
+        /// </value>
+        public string Name { get; set; }
+
+        /// <summary>
+        /// Gets or sets the author.
+        /// </summary>
+        /// <value>
+        /// The author.
+        /// </value>
+        public string Author { get; set; }
+
+        /// <summary>
+        /// Gets or sets the publisher.
+        /// </summary>
+        /// <value>
+        /// The publisher.
+        /// </value>
+        public string Publisher { get; set; }
+
+        /// <summary>
+        /// Gets or sets the volume.
+        /// </summary>
+        /// <value>
+        /// The volume.
+        /// </value>
+        public int Volume { get; set; }
+
+        /// <summary>
+        /// Gets or sets the price.
+        /// </summary>
+        /// <value>
+        /// The price.
+        /// </value>
+        public int Price { get; set; }
+
+        /// <summary>
+        /// Gets or sets a value indicating whether this instance is available.
+        /// </summary>
+        /// <value>
+        ///   <c>true</c> if this instance is available; otherwise, <c>false</c>.
+        /// </value>
+        public bool IsAvailable { get; set; }
+    }
+}
